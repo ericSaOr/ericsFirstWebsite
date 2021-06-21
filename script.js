765e7aa4573914416579d1a4a16bf5e6dc6ed458
Eric Sanchez <ericfsod@gmail.com>
2021-06-21 04:24:10 -0400

    added button and message via javascript

--- script.js
+++ script.js
@@ -1,4 +1,28 @@
-function date(){
-    document.getElementById("date")
-}
 
+
+
+//  function mouseOver() {
+//     document.getElementById("headerDos").addEventListener("mouseover", mouseOver);
+//      document.getElementById("headerDos").style.color = "red";
+//  }
+
+//  function mouseOut() {
+//     document.getElementById("headerDos").addEventListener("mouseover", mouseOver);
+//      document.getElementById("headerDos").style.color"black";
+//  }
+const h3 = document.createElement('h3');
+h3.textContent = "This content added by JavaScript";
+
+
+function buttonEventListener() {
+    const input = document.getElementById('input');
+
+    function buttonAlert() {
+      alert('Alakazam!');
+    }
+    
+    input.addEventListener('click', buttonAlert);
+    
+      }
+
+      buttonEventListener()
